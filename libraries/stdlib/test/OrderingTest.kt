91183b43d8488ee4d6635c728536d57253a28ffd
Ilya Ryzhenkov <orangy@jetbrains.com>
2014-11-07 17:07:35 +0300

    Cleanup ordering, improve API

---
+++ libraries/stdlib/test/OrderingTest.kt
@@ -0,0 +1,64 @@
+package test.compare
+
+import java.util.*
+import kotlin.test.*
+import org.junit.Test
+
+data class Item(val name: String, val rating: Int) : Comparable<Item> {
+    public override fun compareTo(other: Item): Int {
+        return compareValuesBy(this, other, { it.rating }, { it.name })
+    }
+}
+
+class OrderingTest {
+    val v1 = Item("wine", 9)
+    val v2 = Item("beer", 10)
+
+    Test fun compareByCompareTo() {
+        val diff = v1.compareTo(v2)
+        assertTrue(diff < 0)
+    }
+
+    Test fun compareByNameFirst() {
+        val diff = compareValuesBy(v1, v2, { it.name }, { it.rating })
+        assertTrue(diff > 0)
+    }
+
+    Test fun compareByRatingFirst() {
+        val diff = compareValuesBy(v1, v2, { it.rating }, { it.name })
+        assertTrue(diff < 0)
+    }
+
+    Test fun compareSameObjectsByRatingFirst() {
+        val diff = compareValuesBy(v1, v1, { it.rating }, { it.name })
+        assertTrue(diff == 0)
+    }
+
+    Test fun sortUsingComparatorHelperMethod() {
+        val comparator = compareBy<Item> { it.rating } thenBy { it.name }
+
+        val diff = comparator.compare(v1, v2)
+        assertTrue(diff < 0)
+        val items = arrayListOf(v1, v2).sortBy(comparator)
+        assertEquals(v1, items[0])
+        assertEquals(v2, items[1])
+    }
+
+    Test fun sortUsingCustomComparator() {
+        val comparator = object : Comparator<Item> {
+            override fun compare(o1: Item, o2: Item): Int {
+                return compareValuesBy(o1, o2, { it.name }, { it.rating })
+            }
+
+            override fun equals(other: Any?): Boolean {
+                return this == other
+            }
+        }
+        val diff = comparator.compare(v1, v2)
+        assertTrue(diff > 0)
+        val items = arrayListOf(v1, v2).sortBy(comparator)
+        assertEquals(v2, items[0])
+        assertEquals(v1, items[1])
+    }
+
+}
