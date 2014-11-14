805157cd6dfacd6efe658964183c3324464b4fdc
Pavel V. Talanov <talanov.pavel@gmail.com>
2014-11-14 20:03:31 +0300

    ~ tests

--- idea/testData/decompiler/stubBuilder/Types/Types.kt
+++ idea/testData/decompiler/stubBuilder/Types/Types.kt
@@ -1,5 +1,10 @@
 package test
 
-class Types {
-    val g: Int? = null
+abstract class Types {
+    val nullable: Int? = null
+    abstract val list: List<Int>
+    abstract val map: Map<Int, Int>
+    abstract val nullableMap: Map<Int?, Int?>?
+    abstract val projections: Map<in Int, out String>
+//    val function: () -> Unit = {}
 }
