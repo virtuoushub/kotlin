57b53d01b9ede05a174fb917969a6ffa66a4735c
Pavel V. Talanov <talanov.pavel@gmail.com>
2014-11-14 18:05:13 +0300

    ~ test

--- idea/testData/decompiler/stubBuilder/TopLevelMembersPackage/TopLevelMembers.kt
+++ idea/testData/decompiler/stubBuilder/TopLevelMembersPackage/TopLevelMembers.kt
@@ -11,3 +11,8 @@ private fun funWithParams(c: Int) {
 public val immutable: Double = 0.0
 
 public var mutable: Float = 0.0f
+
+public val String.ext: String
+    get() = this
+
+public fun Int.ext(): Int = this + 3
