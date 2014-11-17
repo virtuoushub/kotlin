bc3966bd8fb704cd452d66857216bd0f7af11cee
Pavel V. Talanov <talanov.pavel@gmail.com>
2014-11-17 18:54:46 +0300

    ~ test

--- idea/testData/decompiler/stubBuilder/TypeParams/TypeParams.kt
+++ idea/testData/decompiler/stubBuilder/TypeParams/TypeParams.kt
@@ -2,4 +2,7 @@ package test
 
 class TypeParams<T1: Any, T2, T3: (Int) -> Int, T4, T5: Any?> where T1: Any?, T1: Int?, T1: Int, T2: String {
 
+    fun useParams(p1: T1, p2: T2, p3: T3, p4: T4, P5: T5) {
+    }
+
 }
