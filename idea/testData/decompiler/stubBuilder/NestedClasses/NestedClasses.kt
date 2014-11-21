631a7a8cac3e5ad692b77ea2a1fd227e73173b69
Pavel V. Talanov <talanov.pavel@gmail.com>
2014-11-21 19:45:47 +0300

    ~ test

--- idea/testData/decompiler/stubBuilder/NestedClasses/NestedClasses.kt
+++ idea/testData/decompiler/stubBuilder/NestedClasses/NestedClasses.kt
@@ -1,30 +1,36 @@
 package test
 
-class NestedClasses<TOuter> {
+class NestedClasses {
     fun f() {
     }
 
+    val c: Int = 0
+
     private class Nested<TN> {
-        fun f(p1: TN) {
+        fun f(/*p1: TN*/) {
         }
+
+        val c = 1
 
         public class NN<TNN> {
-            fun f(p1: TNN) {
+            fun f(/*p1: TNN*/) {
             }
+
+            val c = 1
         }
 
-        inner class NI<TNI : TN> {
-            fun f(p1: TN, p2: TNI) {
+        inner class NI<TNI/* : TN*/> {
+            fun f(/*p1: TN,*/ /*p2: TNI*/) {
             }
         }
     }
 
-    public inner class Inner<TI : TOuter> {
-        fun f() {
+    public inner class Inner<TI /*: TOuter*/> {
+        fun f(/*p1: TI*/) {
         }
 
-        private inner class II {
-            fun f() {
+        private inner class II<TII> {
+            fun f(/*p1: TII,*/ p2: II<NestedClasses>) {
             }
         }
     }
