5acd5f0aae2c9f23b1a026ed2b5a9a932e407679
Zalim Bashorov <Zalim.Bashorov@jetbrains.com>
2014-11-28 23:53:06 +0300

    ~ own logging when load plugin descriptor from jar

--- compiler/cli/src/com/intellij/ide/plugins/PluginManagerCoreProxy.java
+++ compiler/cli/src/com/intellij/ide/plugins/PluginManagerCoreProxy.java
@@ -18,13 +18,21 @@ package com.intellij.ide.plugins;
 
 import com.intellij.openapi.extensions.ExtensionPoint;
 import com.intellij.openapi.extensions.ExtensionsArea;
+import com.intellij.openapi.util.JDOMUtil;
+import com.intellij.openapi.util.io.ZipFileCache;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.util.containers.ContainerUtil;
+import com.intellij.util.xmlb.XmlSerializationException;
+import org.jdom.Document;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
+import java.net.URL;
 import java.util.List;
 import java.util.Set;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 // TODO drop this temporary hack to access to PluginManagerCore methods when CoreApplicationEnvironment got similar features.
 public class PluginManagerCoreProxy {
@@ -37,7 +45,40 @@ public class PluginManagerCoreProxy {
 
     @Nullable
     public static IdeaPluginDescriptorImpl loadDescriptorFromJar(@NotNull File file, @NotNull String fileName) {
-        return PluginManagerCore.loadDescriptorFromJar(file, fileName);
+        //return PluginManagerCore.loadDescriptorFromJar(file, fileName);
+        try {
+            String fileURL = StringUtil.replace(file.toURI().toASCIIString(), "!", "%21");
+            URL jarURL = new URL("jar:" + fileURL + "!/META-INF/" + fileName);
+
+            ZipFile zipFile = ZipFileCache.acquire(file.getPath());
+            try {
+                ZipEntry entry = zipFile.getEntry("META-INF/" + fileName);
+                if (entry != null) {
+                    Document document = JDOMUtil.loadDocument(zipFile.getInputStream(entry));
+                    IdeaPluginDescriptorImpl descriptor = new IdeaPluginDescriptorImpl(file);
+                    descriptor.readExternal(document, jarURL);
+                    System.err.println("$$$ I: Load from" + file);
+                    return descriptor;
+                }
+            }
+            finally {
+                ZipFileCache.release(zipFile);
+            }
+        }
+        catch (XmlSerializationException e) {
+            System.err.println("$$ I: Cannot load " + file + "\n" + e);
+            System.err.println("$$ I: Plugin file " + file.getName() + " contains invalid plugin descriptor file.");
+
+            PluginManagerCore.getLogger().info("Cannot load " + file, e);
+            PluginManagerCore.prepareLoadingPluginsErrorMessage(
+                    "Plugin file " + file.getName() + " contains invalid plugin descriptor file.");
+        }
+        catch (Throwable e) {
+            System.err.println("$$$ I: Cannot load " + file + "\n" + e);
+            PluginManagerCore.getLogger().info("Cannot load " + file, e);
+        }
+
+        return null;
     }
 
     // copied as is from PluginManagerCore#registerExtensionPointsAndExtensions
